db1af2edcf95a9dee18dd5cf52ff52a0dbec535a
Emiliano Nataren <eminataren2002@gmail.com>
2024-04-07 02:36:28 -0600

    mambul

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,17 @@
 "use client";
 import HomeLayout from "@/components/layouts/HomeLayout";
+import Image from "next/image";
 
 export default function Home() {
 
   return (
     <HomeLayout>
       <h1 className={`text-3xl font-semibold text-center`}>
-        Welcome to the app
+        Welcome to Mambul
+        <section className="container mx-auto flex justify-center">
+
+          <Image src="/mabul.png" width={800} height={800} alt="Mambul Logo" className="" />
+        </section>
       </h1>
     </HomeLayout>
   );
